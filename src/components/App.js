e1888725c3ead07298e1f16c3907a47d4c5c0d51
alvalle09 <phredvalle@gmail.com>
2019-08-21 23:51:24 -0500

    music-masters init commit

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+
+import React, { Component } from "react";
+
+class App extends Component {
+  state = { artistQuery: '' };
+
+  updateArtistQuery = event => {
+    console.log('event', event);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Music Masters</h2>
+        <input 
+          onChange={this.updateArtistQuery} 
+          placeholder='Search for an Artist' 
+        />
+        <button>Search</button>        
+      </div>
+    );
+  }
+}
+
+
+export default App;
