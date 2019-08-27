b78819b805c044f391e83b37609f4ad70277faea
alvalle09 <phredvalle@gmail.com>
2019-08-26 23:42:15 -0500

    add Artist component

---
+++ src/components/Artist.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Artist = ({ artist } )  => {
+    const { images, name, followers, genres } = artist;
+
+    return (
+        <div>
+            <h3>{name}</h3>
+            <p>{followers.total}</p>
+            <p>{genres.join(',')}</p>
+            <img src={images[0].url} alt='artist-profile' />
+        </div>
+    )
+}
+
+export default Artist;
+
