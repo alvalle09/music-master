d226e3c0449308a3b490b556303eca8bb0aedd6b
alvalle09 <phredvalle@gmail.com>
2019-08-27 23:19:05 -0500

    setup for audio preview

--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -1,30 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Tracks extends Component {
-    render() {
-        // destructure the props
-        const { tracks } = this.props;
 
-        return (
-            <div>  
-                {
-                    tracks.map(track => {
-                        const { id, name, album } = track;
-
-                        return (
-                            <div key={id}>
-                                <img src={album.images[0].url} alt='track-image' />
-                                <p>{name}</p>
-                            </div>
-                        )
-
-                    })
-                }
-            </div>
-        )
+    playAudio = previewURL => {
+        const audio = new Audio(previewURL);
     }
 
+
+  render() {
+    // destructure the props object into tracks object
+    const { tracks } = this.props;
+
+    return (
+      <div>
+        {tracks.map(track => {
+          const { id, name, album, preview_url } = track;
+
+          return (
+            <div key={id}>
+              <img src={album.images[0].url} alt="track-image" />
+              <p>{name}</p>
+
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default Tracks;
-
